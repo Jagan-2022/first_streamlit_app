1b72a65c4d65c3a7a9e024ed2ec0fbb2146f723a
Jagan-2022 <110095292+Jagan-2022@users.noreply.github.com>
2022-07-29 06:42:14 +0530

    Update streamlit_app.py
    api call search box

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #Table displayed on the screen
 streamlit.dataframe(fruityvice_normalized)
+
+#Text search for api call
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
